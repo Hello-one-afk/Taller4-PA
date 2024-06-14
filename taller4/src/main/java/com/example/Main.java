38b1a03795d42c620a2e8555b135711c2ff8c076
Hello-one-afk <110000203+Hello-one-afk@users.noreply.github.com>
2024-06-14 12:18:38 -0400

    feat FizzBuzz creacion de funcionalidades
    -Se crearon los metodos para el juego FizzBuzz

--- taller4/src/main/java/com/example/Main.java
+++ taller4/src/main/java/com/example/Main.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import com.example.ejercicios.FizzBuzz;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        fizzBuzz.game();
     }
 }
--- taller4/src/main/java/com/example/ejercicios/FizzBuzz.java
+++ taller4/src/main/java/com/example/ejercicios/FizzBuzz.java
@@ -1,3 +1,36 @@
 package com.example.ejercicios;
 
-public class FizzBuzz {}
+import java.util.Scanner;
+
+public class FizzBuzz {
+    Scanner teclado = new Scanner(System.in);
+
+    private int numeroUsuario;
+
+    public void game() {
+        usuarioSeleccionarNumero();
+        System.out.println(condicionesFizzBuzz());
+    }
+
+    public void usuarioSeleccionarNumero() {
+        do {
+            System.out.println("Ingrese Numero del 1-100");
+            numeroUsuario = teclado.nextInt();
+        } while (numeroUsuario < 0 || numeroUsuario > 100);
+    }
+
+
+
+    public String condicionesFizzBuzz() {
+        if (numeroUsuario%3==0 && numeroUsuario%5 == 0) {
+            return "FizzBuzz";
+        }else if (numeroUsuario%3==0) {
+            return "Fizz";        
+        }else if (numeroUsuario%5 == 0) {
+            return "Buzz";
+        }
+        return "No se encontro un valor";
+        
+    }
+
+}
